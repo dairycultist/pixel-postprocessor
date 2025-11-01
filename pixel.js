6cd82112e005f9716ecc1c171bf1c81b4f6e7b59
cultist <75032462+dairycultist@users.noreply.github.com>
2025-10-31 23:39:52 -0400

    Add files via upload

--- pixel.js
+++ pixel.js
@@ -17,7 +17,7 @@ function is_transparent(image, x, y) {
 
 function color_difference(c1, c2) {
 
-	return Math.pow(c1[0] - c2[0], 2) + Math.pow(c1[1] - c2[1], 2) + Math.pow(c1[2] - c2[2], 2);
+	return Math.sqrt(Math.pow(c1[0] - c2[0], 2) + Math.pow(c1[1] - c2[1], 2) + Math.pow(c1[2] - c2[2], 2));
 }
 
 function rgb1_to_hex(r, g, b) {
@@ -26,7 +26,15 @@ function rgb1_to_hex(r, g, b) {
 	g = Math.floor(g * 255);
 	b = Math.floor(b * 255);
 
-	return r * 256 * 256 * 256 + g * 256 * 256 + b * 256 + 255;
+	r = Math.max(0, Math.min(255, r));
+	g = Math.max(0, Math.min(255, g));
+	b = Math.max(0, Math.min(255, b));
+
+	// Combine the R, G, and B values using bitwise left shifts and OR operations
+	// R is shifted 16 bits to the left to occupy the higher byte positions
+	// G is shifted 8 bits to the left to occupy the middle byte positions
+	// B occupies the lowest byte positions
+	return (((r << 16) | (g << 8) | b) * 256) + 255;
 }
 
 function surface_change(image_in) {
@@ -38,8 +46,8 @@ function surface_change(image_in) {
 
 			if (!is_transparent(image_in, x, y)) {
 
-				let horizontal = color_difference(get_rgb(image_in, x - 1, y), get_rgb(image_in, x + 1, y));
-				let vertical = color_difference(get_rgb(image_in, x, y - 1), get_rgb(image_in, x, y + 1));
+				let horizontal = color_difference(get_rgb1(image_in, x - 1, y), get_rgb1(image_in, x + 1, y));
+				let vertical = color_difference(get_rgb1(image_in, x, y - 1), get_rgb1(image_in, x, y + 1));
 
 				image_out.setPixelColor(rgb1_to_hex(horizontal / 2 + 0.5, vertical / 2 + 0.5, 1), x, y);
 			}
@@ -49,7 +57,7 @@ function surface_change(image_in) {
 	return image_out;
 }
 
-function stochastic_color_blobbing(image_in, kernel_size, iterations, merge_factor) {
+function stochastic_color_blobbing(image_in, kernel_size, iterations, reduction) {
 
 	for (let i = 0; i < iterations; i++) {
 
@@ -70,7 +78,7 @@ function stochastic_color_blobbing(image_in, kernel_size, iterations, merge_fact
 
 				const neighbor_color = get_rgb1(image_in, x + dx, y + dy);
 
-				if (color_difference(center_color, neighbor_color) < merge_factor) {
+				if (color_difference(center_color, neighbor_color) < reduction) {
 					sum_color[0] += neighbor_color[0];
 					sum_color[1] += neighbor_color[1];
 					sum_color[2] += neighbor_color[2];
@@ -91,10 +99,10 @@ function stochastic_color_blobbing(image_in, kernel_size, iterations, merge_fact
 
 async function run() {
 
-	let img = (await Jimp.read("input.jpg")).resize({ w: 256, h: 256 });
+	let img = (await Jimp.read("input.png")).resize({ w: 256, h: 256 });
 
-	img = stochastic_color_blobbing(img, 4, 256 * 256 * 10, 0.007);
-	img = surface_change(img);
+	img = stochastic_color_blobbing(img, 4, 256 * 256 * 10, 0.04);
+	// img = surface_change(img);
 	
 	await img.write("output.png");
 }
